23d4c89d9a4f59fc35aa0de1a30887cca7c78831
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-07-15 14:05:43 +0100

    Render the episodes on the page as a table

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ app.get("/results", (req, res) => {
   request("https://gist.githubusercontent.com/thekiwi/ab70294c8d7ab790d9b6d70df9d3d145/raw/14513c7b841b37b2406dda4d3b9143a25700a68e/silicon-valley.json", (error, response, body) => {
     if(!error && response.statusCode === 200 ){
       var data = JSON.parse(body);
-      res.render("results");
-      // res.render("results", { data: data})
+      res.render("results", { data: data})
     }
   })
 });
