3ee5258164174954144e507ab5f7a8c0b5bd40bf
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-07-15 12:44:02 +0100

    Render the name on the page

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var app = express();
 app.get("/results", (req, res) => {
   request("https://gist.githubusercontent.com/thekiwi/ab70294c8d7ab790d9b6d70df9d3d145/raw/14513c7b841b37b2406dda4d3b9143a25700a68e/silicon-valley.json", (error, response, body) => {
     if(!error && response.statusCode === 200 ){
-      res.send(body);
+      var data = JSON.parse(body);
+      res.send(data["name"])
+      // res.render("results", { data: data})
     }
   })
 });
