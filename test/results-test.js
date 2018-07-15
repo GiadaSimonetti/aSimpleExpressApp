8db9fe5caa8a6e70535be06c60c529b71facf7dc
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-07-15 15:19:04 +0100

    Pass test that mock data from the API

--- test/results-test.js
+++ test/results-test.js
@@ -9,12 +9,36 @@ chai.use(chaiHttp);
 describe('Results', () => {
   let results = chai.request.agent(app);
 
+  let mockData = {
+        season: '1',
+        number: '1',
+        name: 'mockTitle'
+      }
+
+  beforeEach((done) => {
+    results
+    .post('/code')
+    .send(mockData)
+    .end((err, res) => {
+      done();
+    });
+  });
+
   it('shows the page title', () => {
     results
     .get('/results')
     .end((err, res) => {
       res.should.have.status(200);
       res.text.should.contain('All the Silincon Valley episodes');
+    });
+  });
+
+  it('shows the data from the JSON file', () => {
+    results
+    .get('/results')
+    .end((err, res) => {
+      res.should.have.status(200);
+      res.text.should.contain('mockTitle');
     });
   });
 
