2ca580f7f323671011dd51bd36f9b7649c6b2dd0
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-07-15 12:50:47 +0100

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # aSimpleExpressApp
+
+## Instructions
+
+Prerequisites
+node installed
+
+- to run the web app
+
+```sh
+clone the repo:
+$ git clone https://github.com/GiadaSimonetti/aSimpleExpressApp
+
+change directory:
+  $ cd aSimpleExpressApp
+
+install dependencies:
+  $ npm install
+
+run the app:
+  $ node app.js
+
+```
--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var request = require("request");
 
 var app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/results", (req, res) => {
   request("https://gist.githubusercontent.com/thekiwi/ab70294c8d7ab790d9b6d70df9d3d145/raw/14513c7b841b37b2406dda4d3b9143a25700a68e/silicon-valley.json", (error, response, body) => {
     if(!error && response.statusCode === 200 ){
       var data = JSON.parse(body);
-      res.send(data["name"])
+      res.render("results");
       // res.render("results", { data: data})
     }
   })
