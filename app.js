a9a8856c5b8af187a851e8d7e40a50a7bd358396
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-07-15 14:46:33 +0100

    Return all the episode with title, season, number, image and summary

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var app = express();
 
 app.set("view engine", "ejs");
 
+app.use(express.static(__dirname + '/public'));
+
 app.get("/results", (req, res) => {
   request("https://gist.githubusercontent.com/thekiwi/ab70294c8d7ab790d9b6d70df9d3d145/raw/14513c7b841b37b2406dda4d3b9143a25700a68e/silicon-valley.json", (error, response, body) => {
     if(!error && response.statusCode === 200 ){
