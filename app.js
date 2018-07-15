30fe76f3625a9f779b0141a1ebf219dc74793457
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-07-15 15:46:36 +0100

    Create jshintrc file

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + '/public'));
 
+app.get("/", (req, res) => {
+  res.render("search")
+});
+
 app.get("/results", (req, res) => {
   request("https://gist.githubusercontent.com/thekiwi/ab70294c8d7ab790d9b6d70df9d3d145/raw/14513c7b841b37b2406dda4d3b9143a25700a68e/silicon-valley.json", (error, response, body) => {
     if(!error && response.statusCode === 200 ){
