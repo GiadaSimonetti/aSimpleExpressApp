5529e8c0c164802cfaca22dc04853ded16f5db1c
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-07-15 12:19:33 +0100

    Send the JSON data to the results page

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var request = require("request");
 
 var app = express();
 
-app.get("/results", function(req, res){
-  res.send("hello it works!")
+app.get("/results", (req, res) => {
+  request("https://gist.githubusercontent.com/thekiwi/ab70294c8d7ab790d9b6d70df9d3d145/raw/14513c7b841b37b2406dda4d3b9143a25700a68e/silicon-valley.json", (error, response, body) => {
+    if(!error && response.statusCode === 200 ){
+      res.send(body);
+    }
+  })
 });
 
 app.listen(3000, function(){
